28a1ce03035d3e6b767ca0d96c32971bd6d1c5bf
AshakaE <ashakaegerega@gmail.com>
2021-06-03 06:36:47 +0100

    Add tests for Mealform component

---
+++ src/__test__/MealForm.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import mealReducer from '../reducers/meal';
+import recipeReducer from '../reducers/recipe';
+import filterReducer from '../reducers/filter';
+import MealForm from '../components/MealForm';
+
+const middleware = [thunk];
+const store = createStore(
+  combineReducers({
+    mealState: mealReducer,
+    recipeState: recipeReducer,
+    filterState: filterReducer,
+  }),
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
+
+describe('MealForm', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <MealForm
+          store={store}
+          filters={{
+            q: '',
+            cuisineType: '',
+            dishType: '',
+            mealType: '',
+          }}
+          handleSubmit={() => {}}
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
